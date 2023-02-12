8c4d0f41d564aefb5883bb319c5693bb5a1b0fd5
akhil <akhil@192.168.1.48>
2023-02-12 19:33:39 +0530

    Commiting junior test 5

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -5,5 +5,6 @@ public class Test2 {
 		System.out.println("Junior test");
 		System.out.println("Junior test 2");
 		System.out.println("Junior test 3");
+		System.out.println("Junior test 5");
 	}
 }
